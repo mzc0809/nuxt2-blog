636523e8ba2086528ef0c3d0ccdaf1d2451fba60
mzc0809 <101164674+mzc0809@users.noreply.github.com>
2022-03-22 09:02:58 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 export default {
-  title: 'yun yu yuan',
-  githubName: 'yunyuyuan',
+  title: 'sp create',
+  githubName: 'mzc0809',
   githubRepo: 'nuxt2-blog',
   githubBranch: 'master',
   domain: 'https://blog.halberd.cn',
-  SEO_description: '云与原的博客，书写值得书写的东西',
-  SEO_keywords: 'yunyuyuan,yunyuyuan\'s blog,云与原,云与原的博客',
+  SEO_description: 'spcreate的博客，书写值得书写的东西',
+  SEO_keywords: 'spcreate,spcreate\'s blog,spcreate,spcreate的博客',
   CloudflareAnalyze: "92cdf13c9c964ad1ab71c638c6fde0fc"
 }
